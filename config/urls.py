6ba0435a0b144228f1d953a2b2162ebaa12af1c0
KenArsen <kenzhegulov2002@gmail.com>
2024-02-17 14:06:44 +0600

    move swagger to config/urls

--- config/urls.py
+++ config/urls.py
@@ -5,25 +5,55 @@ from django.urls import path, include
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 class HealthCheckView(APIView):
+    @swagger_auto_schema(
+        tags=["HealthCheck"],
+        operation_summary="Health Check",
+        operation_description="Check the health status of the application",
+        security=[],
+        responses={200: openapi.Response(description="OK")},
+    )
     def get(self, request, *args, **kwargs):
         return Response({"status": "ok"}, status=status.HTTP_200_OK)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('apps.api.urls')),
-    path('api/v1/healthcheck/', HealthCheckView.as_view(), name='healthcheck')
+    path("admin/", admin.site.urls),
+    path("api/", include("apps.api.urls")),
+    path("api/v1/healthcheck/", HealthCheckView.as_view(), name="healthcheck"),
+]
+
+# swagger
+urlpatterns += [
+    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 if settings.DEBUG:
     # debug toolbar
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
     # silk
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
-
-# static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
+    # static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
