671751951538a034c1b3989e147c3c23ef20acb1
KenArsen <kenzhegulov2002@gmail.com>
2024-02-19 19:28:21 +0600

    added STATICFILES_DIRS

--- apps/image/api/v1/urls.py
+++ apps/image/api/v1/urls.py
@@ -1,31 +1,10 @@
-<<<<<<< HEAD
-from rest_framework import routers
-from django.urls import path, include
-=======
 from django.urls import include, path
 from rest_framework import routers
 
->>>>>>> 24677ec (added STATICFILES_DIRS)
 from apps.image import apis
 
 router = routers.DefaultRouter()
 
-<<<<<<< HEAD
-router.register(r'', apis.ImageViewSet)
-
-urlpatterns = [
-    path('', include(router.urls))
-]
-
-# urlpatterns = [
-#     path('', apis.ImageListView.as_view(), name='image-list'),
-#     path('create/', apis.ImageCreateView.as_view(), name='image-create'),
-#     path('<int:pk>/', apis.ImageDetailView.as_view(), name='image-detail'),
-#     path('<int:pk>/update/', apis.ImageUpdateView.as_view(), name='image-update'),
-#     path('<int:pk>/delete/', apis.ImageDeleteView.as_view(), name='image-delete'),
-# ]
-=======
 router.register(r"", apis.ImageViewSet)
 
 urlpatterns = [path("", include(router.urls))]
->>>>>>> 24677ec (added STATICFILES_DIRS)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
-=======
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
->>>>>>> 24677ec (added STATICFILES_DIRS)
 from django.db import models
 from apps.common.base import BaseModel
 from django.utils.translation import gettext_lazy as _
@@ -10,16 +6,9 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
-<<<<<<< HEAD
-
-    def create_user(self, email, password=None, **extra_fields):
-        if email is None:
-            raise TypeError('Users must have an email address.')
-=======
     def create_user(self, email, password=None, **extra_fields):
         if email is None:
             raise TypeError("Users must have an email address.")
->>>>>>> 24677ec (added STATICFILES_DIRS)
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.password = make_password(password)  # Используем make_password для шифрования пароля
@@ -28,11 +17,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password=None):
         if password is None:
-<<<<<<< HEAD
-            raise TypeError('Superusers must have a password.')
-=======
             raise TypeError("Superusers must have a password.")
->>>>>>> 24677ec (added STATICFILES_DIRS)
 
         user = self.create_user(email, password)
         user.is_superuser = True
@@ -42,31 +27,18 @@ class UserManager(BaseUserManager):
         return user
 
 
-<<<<<<< HEAD
-class User(BaseModel, AbstractBaseUser, PermissionsMixin, ):
-=======
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
->>>>>>> 24677ec (added STATICFILES_DIRS)
     email = models.EmailField(max_length=255, unique=True, db_index=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-<<<<<<< HEAD
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-=======
     USERNAME_FIELD = "email"
     EMAIL_FIELD = "email"
->>>>>>> 24677ec (added STATICFILES_DIRS)
 
     objects = UserManager()
 
     class Meta:
-<<<<<<< HEAD
-        ordering = ('-id',)
-=======
         ordering = ("-id",)
->>>>>>> 24677ec (added STATICFILES_DIRS)
         verbose_name = _("User")
         verbose_name_plural = _("Users")
 
