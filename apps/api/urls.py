f152bbf72e477d34fa74d0d89c062472a230bdce
KenArsen <kenzhegulov2002@gmail.com>
2024-02-27 23:29:58 +0600

    Added api path for static and media

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.authentication import SessionAuthentication
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,6 +26,19 @@ urlpatterns = [
     path("v1/events/", include("apps.event.api.v1.urls", namespace="events")),
     path("v1/images/", include("apps.image.api.v1.urls", namespace="images")),
     path("v1/educations/", include("apps.education.api.v1.urls", namespace="educations")),
+]
+
+urlpatterns += [
+    re_path(
+        r"^static/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.STATIC_ROOT, "show_indexes": settings.DEBUG},
+    ),
+    re_path(
+        r"^api/v1/media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT, "show_indexes": settings.DEBUG},
+    ),
 ]
 
 # swagger
