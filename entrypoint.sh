2f67cd2c883e6844b90979b9b546294ded4462a9
KenArsen <kenzhegulov2002@gmail.com>
2024-02-28 17:37:25 +0600

    Created new super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,9 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 exec /bin/sh -c "gunicorn --bind :8000 config.wsgi:application"
 
+
+# Create super user
+python manage.py shell -c "from apps.user.models import User; \
+User.objects.create_superuser('albukhari@gmail.com', 'albukhari2024') if not User.objects.filter(email='albukhari@gmail.com').exists() else None"
+
 chmod +x entrypoint.sh
