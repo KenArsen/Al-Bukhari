d85cc90f7c351ff7b8b04c6bb276ea969ea5a710
KenArsen <kenzhegulov2002@gmail.com>
2024-03-18 08:55:51 +0600

    Added email field on user update serializer

--- apps/user/api/v1/serializers/user_serializer.py
+++ apps/user/api/v1/serializers/user_serializer.py
@@ -43,6 +43,7 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
         instance.first_name = validated_data.get("first_name", instance.first_name)
         instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.is_active = validated_data.get("is_active", instance.is_active)
