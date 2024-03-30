ba5334fc3b12f27ad26f0a1b094c0d5f9d7ab7ab
KenArsen <kenzhegulov2002@gmail.com>
2024-03-30 11:59:22 +0600

    Added filter for list namaz begin

--- apps/education/api/v1/apis/namaz_begin_apis.py
+++ apps/education/api/v1/apis/namaz_begin_apis.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, exceptions
 from drf_yasg.utils import swagger_auto_schema
 
 from apps.education.models import NamazBegin
@@ -6,16 +6,26 @@ from apps.education.api.v1.serializers import NamazBeginSerializer
 
 
 class NamazBeginListAPI(generics.ListAPIView):
-    queryset = NamazBegin.objects.all()
     serializer_class = NamazBeginSerializer
 
     @swagger_auto_schema(
         responses={200: NamazBeginSerializer(many=True)},
         tags=["Namaz Begin"],
-        operation_summary="List Namaz",
+        operation_summary="List Namaz Begin",
     )
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def get_queryset(self):
+        queryset = NamazBegin.objects.all()
+        namaz_type = self.request.data.get("namaz_type", None)
+        gender = self.request.data.get("gender", None)
+
+        if namaz_type and gender:
+            return queryset.filter(namaz_type=namaz_type, gender=gender)
+        elif gender:
+            if namaz_type is None:
+                raise exceptions.ValidationError({"error": "namaz_type parameter is required"})
+            return queryset.filter(namaz_type=namaz_type)
+        else:
+            return queryset
 
 
 class NamazBeginDetailAPI(generics.RetrieveAPIView):
@@ -25,7 +35,7 @@ class NamazBeginDetailAPI(generics.RetrieveAPIView):
     @swagger_auto_schema(
         responses={200: NamazBeginSerializer(many=True)},
         tags=["Namaz Begin"],
-        operation_summary="Retrieve Namaz",
+        operation_summary="Retrieve Namaz Begin",
     )
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -38,7 +48,7 @@ class NamazBeginCreateAPI(generics.CreateAPIView):
     @swagger_auto_schema(
         responses={200: NamazBeginSerializer(many=True)},
         tags=["Namaz Begin"],
-        operation_summary="Create Namaz",
+        operation_summary="Create Namaz Begin",
     )
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
@@ -51,7 +61,7 @@ class NamazBeginUpdateAPI(generics.UpdateAPIView):
     @swagger_auto_schema(
         responses={200: NamazBeginSerializer(many=True)},
         tags=["Namaz Begin"],
-        operation_summary="Update Namaz",
+        operation_summary="Update Namaz Begin",
     )
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
@@ -59,7 +69,7 @@ class NamazBeginUpdateAPI(generics.UpdateAPIView):
     @swagger_auto_schema(
         responses={200: NamazBeginSerializer()},
         tags=["Namaz Begin"],
-        operation_summary="Update Namaz",
+        operation_summary="Update Namaz Begin",
     )
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
