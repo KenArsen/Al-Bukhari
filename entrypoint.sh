297797aecb84770efba676728cb822fd613aa6b6
KenArsen <kenzhegulov2002@gmail.com>
2024-04-03 16:07:06 +0600

    Add sudo apt autclean

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Очистка кэша APT
+sudo apt autoclean
+sudo apt autoremove
+
 python manage.py migrate
 python manage.py collectstatic --no-input
-celery -A config worker -l info &
+#celery -A config worker -l info &
 
 #python manage.py shell -c "from apps.user.models import User; \
 #User.objects.create_superuser(email='admin@gmail.com', password='admin') if not User.objects.filter(email='admin@gmail.com').exists() else None"
 
-#celery -A config worker -l info --without-gossip --without-mingle --without-heartbeat &
+celery -A config worker -l info --without-gossip --without-mingle --without-heartbeat &
 exec /bin/sh -c "gunicorn --bind :8000 config.wsgi:application"
 
 chmod +x entrypoint.sh
