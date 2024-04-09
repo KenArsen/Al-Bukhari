f812816927fc337840c5f119305ec643ef2d32c3
KenArsen <kenzhegulov2002@gmail.com>
2024-04-09 13:06:25 +0600

    Changed CustomStorage

--- config/settings.py
+++ config/settings.py
@@ -189,8 +189,6 @@ customColorPalette = [
     {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
 ]
 
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_5_FILE_STORAGE = 'apps.common.utils.CkeditorCustomStorage'
 CKEDITOR_5_CONFIGS = {
