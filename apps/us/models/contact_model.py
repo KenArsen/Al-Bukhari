ee42396b9b0c540bdd4f1680706c49e9643f8352
KenArsen <kenzhegulov2002@gmail.com>
2024-04-11 10:18:57 +0600

    Alter field icon on url

--- apps/us/models/contact_model.py
+++ apps/us/models/contact_model.py
@@ -7,7 +7,7 @@ class Url(models.Model):
     type_url = models.CharField(max_length=255, blank=True, null=True)
     title = models.CharField(max_length=255)
     url = models.URLField(max_length=255, blank=True, null=True)
-    icon = models.ImageField(upload_to="icons/", blank=True, null=True)
+    icon = models.ImageField(upload_to="us/icons/", blank=True, null=True)
     contact = models.ForeignKey("Contact", on_delete=models.CASCADE, related_name="urls")
 
     def __str__(self):
