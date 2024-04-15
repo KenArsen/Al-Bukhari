e17d6701e5d27ab1f33cc2d0014d9694b721cc31
KenArsen <kenzhegulov2002@gmail.com>
2024-04-15 12:16:55 +0600

    Modified user admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from apps.user.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    list_display_links = ('id', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('id', 'created_at', 'updated_at')
